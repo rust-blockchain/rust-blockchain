97ed979bbf0a2df549e10daa6aa09fd6e0b2853b
Wei Tang <wei@pacna.org>
2024-03-22 05:35:57 +0100

    Simple libp2p behaviour

--- blocknet/src/libp2p.rs
+++ blocknet/src/libp2p.rs
@@ -1,3 +1,19 @@
+use libp2p::{
+    gossipsub, identify, kad, mdns,
+    swarm::{NetworkBehaviour, Swarm},
+};
 use std::borrow::Cow;
 
 pub type ProtocolName = Cow<'static, str>;
+
+#[derive(NetworkBehaviour)]
+struct Behaviour {
+    gossipsub: gossipsub::Behaviour,
+    kademlia: kad::Behaviour<kad::store::MemoryStore>,
+    identify: identify::Behaviour,
+    mdns: mdns::tokio::Behaviour,
+}
+
+pub struct NetworkWorker {
+    swarm: Swarm<Behaviour>,
+}
