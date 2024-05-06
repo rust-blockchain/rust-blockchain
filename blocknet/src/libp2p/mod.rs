018b259ab7258a13c21f2d2b4cb28f4e73aa08a7
Wei Tang <wei@pacna.org>
2024-05-06 23:10:45 +0200

    blocknet/libp2p: AnyMessage/AnyRequest/AnyResponse

--- blocknet/src/libp2p/mod.rs
+++ blocknet/src/libp2p/mod.rs
@@ -9,7 +9,7 @@ use libp2p::{
     gossipsub, identify, kad, mdns, request_response,
     swarm::{NetworkBehaviour, Swarm},
 };
-use serde::{de::DeserializeOwned, Serialize};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use std::{
     borrow::Cow,
     collections::HashMap,
@@ -19,57 +19,69 @@ use std::{
 pub type PeerId = libp2p::PeerId;
 pub type ProtocolName = Cow<'static, str>;
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct AnyRequest {
+    pub protocol_id: String,
+    pub serialized: Vec<u8>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct AnyResponse {
+    pub protocol_id: String,
+    pub serialized: Vec<u8>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct AnyMessage {
+    pub protocol_name: ProtocolName,
+    pub serialized: Vec<u8>,
+}
+
 #[derive(NetworkBehaviour)]
-struct Behaviour<Info, Req, Res>
+struct Behaviour<Info>
 where
     Info: peer_info::Info + Serialize + DeserializeOwned + Send + 'static,
     Info::Push: Serialize + DeserializeOwned + Send + 'static,
-    Req: Serialize + DeserializeOwned + Send + 'static,
-    Res: Serialize + DeserializeOwned + Send + 'static,
 {
     gossipsub: gossipsub::Behaviour,
     kademlia: kad::Behaviour<kad::store::MemoryStore>,
     identify: identify::Behaviour,
     peer_info: peer_info::json::Behaviour<Info>,
     mdns: mdns::tokio::Behaviour,
-    request_response: request_response::json::Behaviour<Req, Res>,
+    request_response: request_response::json::Behaviour<AnyRequest, AnyResponse>,
 }
 
-enum ActionItem<Msg, Req, Res> {
+enum ActionItem {
     Broadcast {
-        protocol_name: ProtocolName,
-        message: Msg,
+        message: AnyMessage,
     },
     Notify {
-        protocol_name: ProtocolName,
         peer_id: PeerId,
-        message: Msg,
+        message: AnyMessage,
     },
     Request {
         peer_id: PeerId,
-        request: Req,
-        receiver: mpsc::Receiver<Res>,
+        request: AnyRequest,
+        receiver: mpsc::Receiver<AnyResponse>,
     },
 }
 
 pub enum Error {}
 
-pub struct NetworkWorker<Info, Msg, Req, Res>
+pub struct NetworkWorker<Info>
 where
     Info: peer_info::Info + Serialize + DeserializeOwned + Send + 'static,
     Info::Push: Serialize + DeserializeOwned + Send + 'static,
-    Req: Serialize + DeserializeOwned + Send + 'static,
-    Res: Serialize + DeserializeOwned + Send + 'static,
 {
-    swarm: Swarm<Behaviour<Info, Req, Res>>,
-    queue: mpsc::Receiver<ActionItem<Msg, Req, Res>>,
+    swarm: Swarm<Behaviour<Info>>,
+    queue: mpsc::Receiver<ActionItem>,
 }
 
 pub struct PeerInfo<Extra> {
     extra: Extra,
 }
 
-pub struct NetworkService<Msg, Req, Res, PeerExtraInfo> {
+pub struct NetworkService<PeerExtraInfo> {
     peers: Arc<RwLock<HashMap<PeerId, PeerInfo<PeerExtraInfo>>>>,
-    sender: mpsc::Sender<ActionItem<Msg, Req, Res>>,
+    sender: mpsc::Sender<ActionItem>,
 }
